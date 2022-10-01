3f6be8e0289ab354e3a3bbd38d2c94f068c0fe34
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-01 03:20:48 +0200

    Fix Loader grapple projectile with projectile randomizer

---
+++ RoR2Randomizer/RoR2Randomizer/Patches/ProjectileRandomizer/ProjectileGrappleFix.cs
@@ -0,0 +1,50 @@
+﻿using RoR2;
+using RoR2.Projectile;
+using RoR2Randomizer.RandomizerController.Projectile;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace RoR2Randomizer.Patches.ProjectileRandomizer
+{
+    [PatchClass]
+    static class ProjectileGrappleFix
+    {
+        static void Apply()
+        {
+            On.RoR2.Projectile.ProjectileManager.InitializeProjectile += ProjectileManager_InitializeProjectile;
+        }
+
+        static void Cleanup()
+        {
+            On.RoR2.Projectile.ProjectileManager.InitializeProjectile -= ProjectileManager_InitializeProjectile;
+        }
+
+        static void ProjectileManager_InitializeProjectile(On.RoR2.Projectile.ProjectileManager.orig_InitializeProjectile orig, ProjectileController projectileController, FireProjectileInfo fireProjectileInfo)
+        {
+            orig(projectileController, fireProjectileInfo);
+
+            if (ProjectileRandomizerController.TryGetOriginalProjectilePrefab(fireProjectileInfo.projectilePrefab, out GameObject originalProjectilePrefab))
+            {
+                if (originalProjectilePrefab.GetComponent<ProjectileGrappleController>() && !projectileController.GetComponent<ProjectileGrappleController>())
+                {
+                    const string STATE_MACHINE_NAME = "Hook";
+
+                    EntityStateMachine hookStateMachine = EntityStateMachine.FindByCustomName(projectileController.owner, STATE_MACHINE_NAME);
+                    if (hookStateMachine)
+                    {
+                        if (hookStateMachine.state is EntityStates.Loader.FireHook fireHook)
+                        {
+                            fireHook.SetHookReference(projectileController.gameObject);
+                        }
+                    }
+                    else
+                    {
+                        Log.Warning($"Tried to initialize grapple projectile, but owner has no '{STATE_MACHINE_NAME}' state machine");
+                    }
+                }
+            }
+        }
+    }
+}
