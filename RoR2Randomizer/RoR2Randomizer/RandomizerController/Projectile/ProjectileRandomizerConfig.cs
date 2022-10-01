cec43096a092273748b126e9e13ae3d76b35006e
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-01 03:20:07 +0200

    Add debug controls to Projectile Randomizer

--- RoR2Randomizer/RoR2Randomizer/RandomizerController/Projectile/ProjectileRandomizerConfig.cs
+++ RoR2Randomizer/RoR2Randomizer/RandomizerController/Projectile/ProjectileRandomizerConfig.cs
@@ -1,5 +1,8 @@
 ﻿using BepInEx.Configuration;
+using RoR2;
 using RoR2Randomizer.Configuration;
+using RoR2Randomizer.Configuration.ConfigValue;
+using RoR2Randomizer.RandomizerController.Boss;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,8 +11,18 @@ namespace RoR2Randomizer.RandomizerController.Projectile
 {
     public sealed class ProjectileRandomizerConfig : BaseRandomizerConfig
     {
+#if DEBUG
+        public readonly EnumConfigValue<BossRandomizerController.DebugMode> DebugMode;
+        public readonly StringConfigValue ForcedProjectileIndex;
+#endif
+
         public ProjectileRandomizerConfig(ConfigFile file) : base("Projectile", file)
         {
+#if DEBUG
+            DebugMode = new EnumConfigValue<BossRandomizerController.DebugMode>(getEntry("Debug Mode", BossRandomizerController.DebugMode.None));
+
+            ForcedProjectileIndex = new StringConfigValue(getEntry("Forced Projectile Index", "0"));
+#endif
         }
     }
 }
--- RoR2Randomizer/RoR2Randomizer/RandomizerController/Projectile/ProjectileRandomizerController.cs
+++ RoR2Randomizer/RoR2Randomizer/RandomizerController/Projectile/ProjectileRandomizerController.cs
@@ -4,10 +4,12 @@ using RoR2;
 using RoR2.Projectile;
 using RoR2Randomizer.Configuration;
 using RoR2Randomizer.Networking.ProjectileRandomizer;
+using RoR2Randomizer.RandomizerController.Boss;
 using RoR2Randomizer.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -75,18 +77,36 @@ namespace RoR2Randomizer.RandomizerController.Projectile
             _projectileIndicesReplacements.Dispose();
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static bool getProjectileReplacement(int original, out int replacement)
+        {
+#if DEBUG
+            switch ((BossRandomizerController.DebugMode)ConfigManager.ProjectileRandomizer.DebugMode)
+            {
+                case BossRandomizerController.DebugMode.Manual:
+                    replacement = _forcedProjectileIndex;
+                    return true;
+                case BossRandomizerController.DebugMode.Forced:
+                    replacement = int.Parse(ConfigManager.ProjectileRandomizer.ForcedProjectileIndex);
+                    return true;
+            }
+#endif
+
+            return _projectileIndicesReplacements.Value.TryGetReplacement(original, out replacement);
+        }
+
         public static void TryOverrideProjectilePrefab(ref GameObject prefab)
         {
             if (shouldBeActive && _projectileIndicesReplacements.HasValue)
             {
                 int originalIndex = ProjectileCatalog.GetProjectileIndex(prefab);
-                if (_projectileIndicesReplacements.Value.TryGetReplacement(originalIndex, out int replacementIndex))
+                if (getProjectileReplacement(originalIndex, out int replacementIndex))
                 {
                     GameObject replacementPrefab = ProjectileCatalog.GetProjectilePrefab(replacementIndex);
                     if (replacementPrefab)
                     {
 #if DEBUG
-                        Log.Debug($"Projectile randomizer: Replaced projectile: {prefab} ({originalIndex}) -> {replacementPrefab} ({replacementIndex})");
+                        Log.Debug($"Projectile randomizer: Replaced projectile: {prefab.name} ({originalIndex}) -> {replacementPrefab.name} ({replacementIndex})");
 #endif
 
                         prefab = replacementPrefab;
@@ -95,7 +115,7 @@ namespace RoR2Randomizer.RandomizerController.Projectile
             }
         }
 
-        public static bool TryGetOriginalProjectile(int replacementIndex, out int originalIndex)
+        public static bool TryGetOriginalProjectileIndex(int replacementIndex, out int originalIndex)
         {
             if (shouldBeActive && _projectileIndicesReplacements.HasValue && _projectileIndicesReplacements.Value.TryGetOriginal(replacementIndex, out originalIndex))
             {
@@ -105,5 +125,48 @@ namespace RoR2Randomizer.RandomizerController.Projectile
             originalIndex = -1;
             return false;
         }
+
+        public static bool TryGetOriginalProjectilePrefab(GameObject replacementPrefab, out GameObject originalPrefab)
+        {
+            if (TryGetOriginalProjectileIndex(ProjectileCatalog.GetProjectileIndex(replacementPrefab), out int originalIndex))
+            {
+                originalPrefab = ProjectileCatalog.GetProjectilePrefab(originalIndex);
+                return (bool)originalPrefab;
+            }
+
+            originalPrefab = null;
+            return false;
+        }
+
+#if DEBUG
+        static int _forcedProjectileIndex = 0;
+
+        void Update()
+        {
+            if (ConfigManager.ProjectileRandomizer.Enabled && ConfigManager.ProjectileRandomizer.DebugMode == BossRandomizerController.DebugMode.Manual)
+            {
+                bool changedProjectileIndex = false;
+                if (Input.GetKeyDown(KeyCode.KeypadPlus))
+                {
+                    if (++_forcedProjectileIndex >= ProjectileCatalog.projectilePrefabCount)
+                        _forcedProjectileIndex = 0;
+
+                    changedProjectileIndex = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+                {
+                    if (--_forcedProjectileIndex < 0)
+                        _forcedProjectileIndex = ProjectileCatalog.projectilePrefabCount - 1;
+
+                    changedProjectileIndex = true;
+                }
+
+                if (changedProjectileIndex)
+                {
+                    Log.Debug($"Current projectile override: {ProjectileCatalog.GetProjectilePrefab(_forcedProjectileIndex).name} ({_forcedProjectileIndex})");
+                }
+            }
+        }
+#endif
     }
 }
