96d2956a76b75f1b7cf8ae154dd6393a1cc6f313
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-03 03:02:44 +0200

    Bump version

--- RoR2Randomizer/RoR2Randomizer/Main.cs
+++ RoR2Randomizer/RoR2Randomizer/Main.cs
@@ -30,7 +30,7 @@ namespace RoR2Randomizer
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "Gorakh";
         public const string PluginName = "RoR2Randomizer";
-        public const string PluginVersion = "0.3.2";
+        public const string PluginVersion = "0.3.3";
 
         public static Main Instance { get; private set; }
 
