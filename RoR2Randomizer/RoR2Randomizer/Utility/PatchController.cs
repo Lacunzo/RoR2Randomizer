5ec3678e5174093973c55792ba4464bd390cb640
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-08 16:40:15 +0200

    Removing unnecessary usings

--- RoR2Randomizer/RoR2Randomizer/Utility/PatchController.cs
+++ RoR2Randomizer/RoR2Randomizer/Utility/PatchController.cs
@@ -1,21 +1,8 @@
 ﻿using HarmonyLib;
-using RoR2;
 using RoR2Randomizer.Patches;
 using RoR2Randomizer.RandomizerControllers;
-using RoR2Randomizer.RandomizerControllers.Boss;
-using RoR2Randomizer.RandomizerControllers.Buff;
-using RoR2Randomizer.RandomizerControllers.Effect;
-using RoR2Randomizer.RandomizerControllers.ExplicitSpawn;
-using RoR2Randomizer.RandomizerControllers.Projectile;
-#if !DISABLE_SKILL_RANDOMIZER
-using RoR2Randomizer.RandomizerController.Skill;
-#endif
-using RoR2Randomizer.RandomizerControllers.Stage;
-using RoR2Randomizer.RandomizerControllers.SurvivorPod;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 using UnityEngine;
 
 namespace RoR2Randomizer.Utility
