28d05ceacb28ab98d0e8bb424cdbb70d7190e79e
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-15 22:11:49 +0200

    Added Solus Control Unit minions to summon randomizer

---
+++ RoR2Randomizer/RoR2Randomizer/Patches/ExplicitSpawnRandomizer/RoboBallBossDeployMinion_SpawnHook.cs
@@ -0,0 +1,35 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using RoR2Randomizer.RandomizerControllers.ExplicitSpawn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoR2Randomizer.Patches.ExplicitSpawnRandomizer
+{
+    [PatchClass]
+    static class RoboBallBossDeployMinion_SpawnHook
+    {
+        static void Apply()
+        {
+            IL.EntityStates.RoboBallBoss.Weapon.DeployMinions.SummonMinion += DeployMinions_SummonMinion;
+        }
+
+        static void Cleanup()
+        {
+            IL.EntityStates.RoboBallBoss.Weapon.DeployMinions.SummonMinion -= DeployMinions_SummonMinion;
+        }
+
+        static void DeployMinions_SummonMinion(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(x => x.MatchCallOrCallvirt<DirectorCore>(nameof(DirectorCore.TrySpawnObject))))
+            {
+                c.Emit(OpCodes.Dup);
+                c.EmitDelegate(ExplicitSpawnRandomizerController.ReplaceDirectorSpawnRequest);
+            }
+        }
+    }
+}
