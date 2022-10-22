6c0d45dc5f6e48b0866baff87251b71e86da9ae3
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-22 18:11:48 +0200

    Slight change to Stage Randomizer Starting Stage Blacklist setting description

--- RoR2Randomizer/RoR2Randomizer/RandomizerControllers/Stage/StageRandomizerConfig.cs
+++ RoR2Randomizer/RoR2Randomizer/RandomizerControllers/Stage/StageRandomizerConfig.cs
@@ -16,7 +16,7 @@ namespace RoR2Randomizer.RandomizerControllers.Stage
 
         public StageRandomizerConfig(ConfigFile file) : base("Stage", file)
         {
-            FirstStageBlacklistEnabled = new BoolConfigValue(getEntry("Starting Stage Blacklist", "Ensures the first stage is always normal(ish) (No Commencement, or Voidling fight on stage 1)", true));
+            FirstStageBlacklistEnabled = new BoolConfigValue(getEntry("Starting Stage Blacklist", "Ensures the first stage is always normal(ish) (No run-ending stages will get picked as the first stage)", true));
 
             PossibleFirstStageWeightMult = new SliderConfigValue<float>(getEntry("Normal starting stage weight multiplier", $"If set to 0.50: Stages that have a chance to be selected as the first stage in a run are half as likely to get picked as the first stage by the stage randomizer.\n\nIf set to 0.25: Starting stages have a 4 times smaller chance to get picked as the first stage.\n\nIf set to 1.00: Feature is effectively disabled, and all stages will be equally likely to get picked.\n\nDefault value: {FIRST_STAGE_WEIGHT_MULT_DEFAULT:F2}", FIRST_STAGE_WEIGHT_MULT_DEFAULT), SliderType.StepSlider, 0f, 1.5f, "{0:F2}", 0.05f);
         }
