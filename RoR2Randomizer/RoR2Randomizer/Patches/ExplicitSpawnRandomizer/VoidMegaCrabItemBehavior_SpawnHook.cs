234131c87db4d8e82eaeadf23d3add84f4e02185
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-23 02:52:06 +0200

    Added Void allies spawned by Newly Hatched Zoea to Summon Randomizer

---
+++ RoR2Randomizer/RoR2Randomizer/Patches/ExplicitSpawnRandomizer/VoidMegaCrabItemBehavior_SpawnHook.cs
@@ -0,0 +1,35 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using RoR2Randomizer.RandomizerControllers.ExplicitSpawn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoR2Randomizer.Patches.ExplicitSpawnRandomizer
+{
+    [PatchClass]
+    static class VoidMegaCrabItemBehavior_SpawnHook
+    {
+        static void Apply()
+        {
+            IL.RoR2.VoidMegaCrabItemBehavior.FixedUpdate += VoidMegaCrabItemBehavior_FixedUpdate;
+        }
+
+        static void Cleanup()
+        {
+            IL.RoR2.VoidMegaCrabItemBehavior.FixedUpdate -= VoidMegaCrabItemBehavior_FixedUpdate;
+        }
+
+        static void VoidMegaCrabItemBehavior_FixedUpdate(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(x => x.MatchCallOrCallvirt<DirectorCore>(nameof(DirectorCore.TrySpawnObject))))
+            {
+                c.Emit(OpCodes.Dup);
+                c.EmitDelegate(ExplicitSpawnRandomizerController.ReplaceDirectorSpawnRequest);
+            }
+        }
+    }
+}
