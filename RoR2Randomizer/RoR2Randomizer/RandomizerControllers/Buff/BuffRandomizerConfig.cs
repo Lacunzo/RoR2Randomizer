88742d148c7d9acba07c00adc6cfbcdb245fb0db
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-24 13:48:44 +0200

    Tweak SwapBuffDebuffWeightMult default value

--- RoR2Randomizer/RoR2Randomizer/RandomizerControllers/Buff/BuffRandomizerConfig.cs
+++ RoR2Randomizer/RoR2Randomizer/RandomizerControllers/Buff/BuffRandomizerConfig.cs
@@ -24,7 +24,7 @@ namespace RoR2Randomizer.RandomizerControllers.Buff
         {
             MixBuffsAndDebuffs = new BoolConfigValue(getEntry("Mix buffs and debuffs", "If the randomizer should be able to turn a buff (positive effect) into a debuff (negative effect) and vice versa.", true));
 
-            SwapBuffDebuffWeightMult = new SliderConfigValue<float>(getEntry("Buff-Debuff Weight Multiplier", "The weight multiplier to apply to buffs randomizing into debuffs and vice verse.\n\nThe lower the value, the less likely a buff will be to get randomized into a debuff, and vice versa.", 0.3f), SliderType.StepSlider, 0f, 1f, "F2", 0.05f);
+            SwapBuffDebuffWeightMult = new SliderConfigValue<float>(getEntry("Buff-Debuff Weight Multiplier", "The weight multiplier to apply to buffs randomizing into debuffs and vice verse.\n\nThe lower the value, the less likely a buff will be to get randomized into a debuff, and vice versa.", 0.6f), SliderType.StepSlider, 0f, 1f, "F2", 0.05f);
 
             ExcludeInvincibility = new BoolConfigValue(getEntry("Blacklist Invincibility Effects", "Disables randomization (both from and to) of status effects that make the user invincible. Turning this off can potentially softlock a run due to a boss becoming invincible.", true));
 
