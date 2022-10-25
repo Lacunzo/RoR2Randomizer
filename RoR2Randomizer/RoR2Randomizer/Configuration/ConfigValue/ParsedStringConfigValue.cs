e27d7de312340e284ac3b3f8e97d33366fd1a539
Petter Öhlén <petter.ohlen@gmail.com>
2022-10-25 11:08:26 +0200

    Move class to own file

---
+++ RoR2Randomizer/RoR2Randomizer/Configuration/ConfigValue/ParsedStringConfigValue.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx.Configuration;
+
+namespace RoR2Randomizer.Configuration.ConfigValue
+{
+    public abstract class ParsedStringConfigValue<T> : StringConfigValue
+    {
+        protected readonly T _fallback;
+
+        protected ParsedStringConfigValue(ConfigEntry<string> entry, T fallback) : base(entry)
+        {
+            _fallback = fallback;
+        }
+
+        public T Parsed
+        {
+            get
+            {
+                if (tryParse(Entry.Value, out T parsedValue))
+                {
+                    return parsedValue;
+                }
+                else
+                {
+                    Log.Warning($"Setting {GetSettingPath()} does not have a valid value");
+                    return _fallback;
+                }
+            }
+        }
+
+        protected abstract bool tryParse(string str, out T value);
+    }
+}
