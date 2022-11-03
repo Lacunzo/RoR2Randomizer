078bd251312f3bba6dec8d25636844cf0bfae962
Petter Öhlén <petter.ohlen@gmail.com>
2022-11-03 12:36:19 +0100

    Add Defense Nucleus to summon randomizer

---
+++ RoR2Randomizer/RoR2Randomizer/Patches/ExplicitSpawnRandomizer/ProjectileSpawnMaster_SpawnHook.cs
@@ -0,0 +1,35 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using RoR2Randomizer.RandomizerControllers.ExplicitSpawn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoR2Randomizer.Patches.ExplicitSpawnRandomizer
+{
+    [PatchClass]
+    static class ProjectileSpawnMaster_SpawnHook
+    {
+        static void Apply()
+        {
+            IL.RoR2.Projectile.ProjectileSpawnMaster.SpawnMaster += ProjectileSpawnMaster_SpawnMaster;
+        }
+
+        static void Cleanup()
+        {
+            IL.RoR2.Projectile.ProjectileSpawnMaster.SpawnMaster += ProjectileSpawnMaster_SpawnMaster;
+        }
+
+        static void ProjectileSpawnMaster_SpawnMaster(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(x => x.MatchCallOrCallvirt<DirectorCore>(nameof(DirectorCore.TrySpawnObject))))
+            {
+                c.Emit(OpCodes.Dup);
+                c.EmitDelegate(ExplicitSpawnRandomizerController.ReplaceDirectorSpawnRequest);
+            }
+        }
+    }
+}
