cfce95edfbf19c8f10d64488df83b46ba7e47fd3
Petter Öhlén <petter.ohlen@gmail.com>
2022-11-15 16:14:35 +0100

    Don't use ProjectileManager GameObject as projectile weapon source

--- RoR2Randomizer/RoR2Randomizer/Patches/ProjectileRandomizer/ReplaceFireProjectileServerPrefab.cs
+++ RoR2Randomizer/RoR2Randomizer/Patches/ProjectileRandomizer/ReplaceFireProjectileServerPrefab.cs
@@ -18,7 +18,7 @@ namespace RoR2Randomizer.Patches.ProjectileRandomizer
 
         static void ProjectileManager_FireProjectile_FireProjectileInfo(On.RoR2.Projectile.ProjectileManager.orig_FireProjectile_FireProjectileInfo orig, ProjectileManager self, FireProjectileInfo fireProjectileInfo)
         {
-            if (ProjectileRandomizerController.TryReplaceFire(fireProjectileInfo, self.gameObject))
+            if (ProjectileRandomizerController.TryReplaceFire(fireProjectileInfo, null))
             {
                 return;
             }
