0ab86a8d0b5355df24688af909f81865a26907fe
a22petoh <petter.ohlen@gmail.com>
2023-03-10 01:37:03 +0100

    Fix non-player characters having identical equipment rng

---
+++ RoR2Randomizer/RoR2Randomizer/Patches/ItemRandomizer/NonPlayerEquipmentSlotRNGPatch.cs
@@ -0,0 +1,43 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+
+namespace RoR2Randomizer.Patches.ItemRandomizer
+{
+    [PatchClass]
+    static class NonPlayerEquipmentSlotRNGPatch
+    {
+        static void Apply()
+        {
+            IL.RoR2.EquipmentSlot.Start += EquipmentSlot_Start;
+        }
+
+        static void Cleanup()
+        {
+            IL.RoR2.EquipmentSlot.Start -= EquipmentSlot_Start;
+        }
+
+        static void EquipmentSlot_Start(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(x => x.MatchNewobj<Xoroshiro128Plus>()))
+            {
+                c.Emit(OpCodes.Ldarg_0);
+                c.EmitDelegate((ulong seed, EquipmentSlot instance) =>
+                {
+                    CharacterBody ownerBody = instance.characterBody;
+                    if (ownerBody)
+                    {
+                        if (!ownerBody.isPlayerControlled)
+                        {
+                            seed ^= instance.netId.Value;
+                        }
+                    }
+
+                    return seed;
+                });
+            }
+        }
+    }
+}
