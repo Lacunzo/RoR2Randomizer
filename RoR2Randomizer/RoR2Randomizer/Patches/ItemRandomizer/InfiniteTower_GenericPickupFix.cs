cff51fb8f6e90f3918e2e610fb8a5d75848d571d
a22petoh <petter.ohlen@gmail.com>
2023-03-10 16:35:10 +0100

    Apply generic pickup fix to simulacrum

---
+++ RoR2Randomizer/RoR2Randomizer/Patches/ItemRandomizer/InfiniteTower_GenericPickupFix.cs
@@ -0,0 +1,135 @@
+﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using RoR2;
+using RoR2Randomizer.CustomContent;
+using RoR2Randomizer.Extensions;
+using UnityEngine.Networking;
+
+namespace RoR2Randomizer.Patches.ItemRandomizer
+{
+    [PatchClass]
+    static class InfiniteTower_GenericPickupFix
+    {
+        static void Apply()
+        {
+            IL.RoR2.InfiniteTowerRun.AdvanceWave += InfiniteTowerRun_AdvanceWave;
+            On.RoR2.InfiniteTowerWaveController.OnCombatSquadMemberDiscovered += InfiniteTowerWaveController_OnCombatSquadMemberDiscovered;
+        }
+
+        static void Cleanup()
+        {
+            IL.RoR2.InfiniteTowerRun.AdvanceWave -= InfiniteTowerRun_AdvanceWave;
+            On.RoR2.InfiniteTowerWaveController.OnCombatSquadMemberDiscovered -= InfiniteTowerWaveController_OnCombatSquadMemberDiscovered;
+        }
+
+        static void InfiniteTowerWaveController_OnCombatSquadMemberDiscovered(On.RoR2.InfiniteTowerWaveController.orig_OnCombatSquadMemberDiscovered orig, InfiniteTowerWaveController self, CharacterMaster master)
+        {
+            orig(self, master);
+
+            if (NetworkServer.active)
+            {
+                master.inventory.CopyEquipmentFrom(self.enemyInventory);
+
+                if (master.inventory.currentEquipmentIndex != EquipmentIndex.None)
+                {
+                    master.inventory.GiveItemIfMissing(ContentPackManager.Items.MonsterUseEquipmentDummyItem);
+                }
+            }
+        }
+
+        static void InfiniteTowerRun_AdvanceWave(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            ILCursor[] foundCursors;
+
+            int enemyItemEntryLocalIndex = -1;
+            if (c.TryFindNext(out foundCursors,
+                              x => x.MatchLdfld<InfiniteTowerRun>(nameof(InfiniteTowerRun.enemyItemPattern)),
+                              x => x.MatchLdelemAny<InfiniteTowerRun.EnemyItemEntry>(),
+                              x => x.MatchStloc(out enemyItemEntryLocalIndex)))
+            {
+#if DEBUG
+                Log.Debug($"{nameof(enemyItemEntryLocalIndex)}={enemyItemEntryLocalIndex}");
+#endif
+            }
+            else
+            {
+                Log.Error("Failed to find enemyItemEntry local index");
+                return;
+            }
+
+            int pickupDefLocalIndex = -1;
+            if (c.TryFindNext(out foundCursors,
+                              x => x.MatchCallOrCallvirt(SymbolExtensions.GetMethodInfo(() => PickupCatalog.GetPickupDef(default))),
+                              x => x.MatchStloc(out pickupDefLocalIndex)))
+            {
+#if DEBUG
+                Log.Debug($"{nameof(pickupDefLocalIndex)}={pickupDefLocalIndex}");
+#endif
+            }
+            else
+            {
+                Log.Error("Failed to find pickupDef local index");
+                return;
+            }
+
+            if (c.TryFindNext(out foundCursors,
+                              x => x.MatchLdloc(pickupDefLocalIndex),
+                              x => x.MatchBrfalse(out _)))
+            {
+                ILCursor cursor = foundCursors[1];
+                cursor.Index++;
+
+                cursor.Emit(OpCodes.Ldloc, enemyItemEntryLocalIndex);
+                cursor.Emit(OpCodes.Ldloc, pickupDefLocalIndex);
+                cursor.Emit(OpCodes.Ldarg_0);
+                cursor.EmitDelegate((InfiniteTowerRun.EnemyItemEntry itemEntry, PickupDef pickup, InfiniteTowerRun instance) =>
+                {
+                    if (pickup == null ||
+                        pickup.itemIndex != ItemIndex.None) // If Item: Use default code
+                    {
+                        return false;
+                    }
+
+                    int count;
+                    if (pickup.equipmentIndex != EquipmentIndex.None)
+                    {
+                        count = 1;
+                    }
+                    else
+                    {
+                        count = itemEntry.stacks;
+                    }
+
+                    return pickup.TryGrantTo(instance.enemyInventory, count, true);
+                });
+
+                ILLabel afterGiveItemLabel = il.DefineLabel();
+
+                cursor.Emit(OpCodes.Brtrue, afterGiveItemLabel);
+
+                if (cursor.TryGotoNext(MoveType.After,
+                                       x => x.MatchCallOrCallvirt(SymbolExtensions.GetMethodInfo<Inventory>(_ => _.GiveItem(default(ItemIndex), default)))))
+                {
+                    cursor.MarkLabel(afterGiveItemLabel);
+                }
+                else
+                {
+                    Log.Error($"Failed to find {afterGiveItemLabel} target");
+                    return;
+                }
+            }
+            else
+            {
+                Log.Error("Failed to find patch location");
+                return;
+            }
+
+#if DEBUG
+            Log.Debug(il.ToString());
+#endif
+        }
+    }
+}
